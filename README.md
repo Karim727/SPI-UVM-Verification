e46a12c6f4e0af9fcea448337990bbd4bd2181b0
Karim <waleedkareem293@gmail.com>
2025-10-08 20:31:04 +0300

    deleted duplicate files

--- README.md
+++
@@ -1,1 +0,0 @@
-# SPI-UVM-Verification
--- SPI_if.sv
+++
@@ -1,9 +0,0 @@
-import SPI_shared_pkg::*;
-interface SPI_if (clk);
-  input            clk; 
-  logic            MOSI, rst_n, SS_n, tx_valid;
-  logic      [7:0] tx_data;
-  logic      [9:0] rx_data;
-  logic            rx_valid, MISO;
-  logic      [2:0] cs;
-endinterface : SPI_if
